8159ca1822af8d05ffd69a34badc4d303652b5c9
Toaster1995 <lukasstuffer2908@gmail.com>
2024-01-19 16:17:41 +0100

    Add report func

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 def main():
     book_path = "books/frankenstein.txt"
+    print_report(book_path)
+
+def print_report(book_path):
+    print(f"--- Begin report of {book_path} ---")
+    print()
     text = get_book_text(book_path)
-    print(get_chars_dict(text))
+    words_cnt = count_words(text)
+    print(f"{words_cnt} words found in the  given document")
+    print()
+    
+    letters_dict = get_chars_dict(text)
+    new_list = []
+    for char in letters_dict:
+        if char.isalpha():
+            new_list.append(char)
+            
+    new_list.sort()
+    for char in new_list:
+        cnt = letters_dict[char]
+        print(f"The '{char}' character was found {cnt} times")
+    print()
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -9,7 +29,7 @@ def get_book_text(path):
 
 def count_words(book_text):
     words = book_text.split()
-    print(len(words))
+    return len(words)
 
 def get_chars_dict(book_text):
     res = {}
